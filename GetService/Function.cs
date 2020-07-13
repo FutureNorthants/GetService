dc90a63cfcdaf9ee61a92b62385182194974bbab
Kevin White <kwhite@northampton.gov.uk>
2020-07-13 15:02:52 +0100

    Switched to Step Function handler

--- GetService/Function.cs
+++ GetService/Function.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using System.Diagnostics;
 using System;
@@ -7,6 +6,11 @@ using Amazon.Lex;
 using Amazon;
 using Amazon.Lex.Model;
 using System.Threading.Tasks;
+using Amazon.StepFunctions;
+using Amazon.StepFunctions.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -16,26 +20,89 @@ namespace GetService
     public class Function
     {
         private static readonly RegionEndpoint lexRegion = RegionEndpoint.EUWest2;
+        private static String taskToken;
 
         public String myTest = "Test Ref";
 
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest Request, ILambdaContext context)
+        public async Task FunctionHandler(object input, ILambdaContext context)
         {
-
-            // String response = GetClientFromLexAsync().Result;
-            //Task<String> response = GetClientFromLexAsync();
-            //GetClientFromLexAsync().Wait();
-            GetClientFromLexAsync().GetAwaiter().GetResult();
-
-            APIGatewayProxyResponse apiResponse = new APIGatewayProxyResponse
+            String instance = Environment.GetEnvironmentVariable("instance");
+            String cxmEndPoint;
+            String cxmAPIKey;
+            //TODO Use secrets manager
+            //TODO change default to error trap
+            //TODO Use lambda variable instead of instance
+            switch (instance.ToLower())
             {
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" }, { "Result", "Success" } }
-            };
-            return apiResponse;
+                case "live":
+                    cxmEndPoint = Environment.GetEnvironmentVariable("cxmEndPointLive");
+                    cxmAPIKey = Environment.GetEnvironmentVariable("cxmAPIKeyLive");
+                    break;
+                default:
+                    cxmEndPoint = Environment.GetEnvironmentVariable("cxmEndPointTest");
+                    cxmAPIKey = Environment.GetEnvironmentVariable("cxmAPIKeyTest");
+                    break;
+            }
+            JObject o = JObject.Parse(input.ToString());
+            String caseReference = (string)o.SelectToken("CaseReference");
+            taskToken = (string)o.SelectToken("TaskToken");
+            Console.WriteLine("caseReference : " + caseReference);
 
+            
+
+            CaseDetails caseDetails = await GetCustomerContactAsync(cxmEndPoint, cxmAPIKey, caseReference, taskToken);
+            try
+            {
+                if (!String.IsNullOrEmpty(caseDetails.customerContact))
+                {
+                    String response = await GetClientFromLexAsync(caseDetails.customerContact);
+
+                    if (!String.IsNullOrEmpty(response))
+                    {
+                            await SendSuccessAsync();
+                        Console.WriteLine("Service : " + response);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            Console.WriteLine("Completed");
         }
 
-        static async Task<string> GetClientFromLexAsync()
+        private async Task<CaseDetails> GetCustomerContactAsync(String cxmEndPoint, String cxmAPIKey, String caseReference, String taskToken)
+        {
+            CaseDetails caseDetails = new CaseDetails();
+            HttpClient cxmClient = new HttpClient();
+            cxmClient.BaseAddress = new Uri(cxmEndPoint);
+            String requestParameters = "key=" + cxmAPIKey;
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/api/service-api/norbert/case/" + caseReference + "?" + requestParameters);
+            try
+            {
+                HttpResponseMessage response = cxmClient.SendAsync(request).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    HttpContent responseContent = response.Content;
+                    String responseString = responseContent.ReadAsStringAsync().Result;
+                    JObject caseSearch = JObject.Parse(responseString);
+                    caseDetails.customerContact = (String)caseSearch.SelectToken("values.enquiry_details");
+                }
+                else
+                {
+                    await SendFailureAsync("Getting case details for " + caseReference + " : " + response.StatusCode.ToString());
+                    Console.WriteLine("ERROR : GetStaffResponseAsync : " + request.ToString());
+                    Console.WriteLine("ERROR : GetStaffResponseAsync : " + response.StatusCode.ToString());
+                }
+            }
+            catch (Exception error)
+            {
+                await SendFailureAsync("Getting case details for " + caseReference + " : " + error.Message);
+                Console.WriteLine("ERROR : GetStaffResponseAsync : " + error.StackTrace);
+            }
+            return caseDetails;
+        }
+
+        static async Task<string> GetClientFromLexAsync(String customerContact)
         {
             try
             {
@@ -44,14 +111,69 @@ namespace GetService
                 textRequest.UserId = "MailBot1";
                 textRequest.BotAlias = "CONNIENBC";
                 textRequest.BotName = "Connie_NBC";
-                textRequest.InputText = "How can i claim benefits";
+                textRequest.InputText = customerContact;
                 PostTextResponse textRespone = await lexClient.PostTextAsync(textRequest);
                 return textRespone.IntentName;
             }
             catch(Exception error)
             {
-                return "Error";
+                await SendFailureAsync(error.Message);
+                return "Error" + error.Message;
             }
         }
+
+        private async Task SendSuccessAsync()
+        {
+            AmazonStepFunctionsClient client = new AmazonStepFunctionsClient();
+            SendTaskSuccessRequest successRequest = new SendTaskSuccessRequest();
+            successRequest.TaskToken = taskToken;
+            Dictionary<String, String> result = new Dictionary<String, String>
+            {
+                { "Result"  , "Success"  },
+                { "Message" , "Completed"}
+            };
+
+            string requestOutput = JsonConvert.SerializeObject(result, Formatting.Indented);
+            successRequest.Output = requestOutput;
+            try
+            {
+                await client.SendTaskSuccessAsync(successRequest);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("ERROR : SendSuccessAsync : " + error.Message);
+                Console.WriteLine("ERROR : SendSuccessAsync : " + error.StackTrace);
+            }
+            await Task.CompletedTask;
+        }
+
+        private static async Task SendFailureAsync(String message)
+        {
+            AmazonStepFunctionsClient client = new AmazonStepFunctionsClient();
+            SendTaskSuccessRequest successRequest = new SendTaskSuccessRequest();
+            successRequest.TaskToken = taskToken;
+            Dictionary<String, String> result = new Dictionary<String, String>
+            {
+                { "Result"  , "Error" },
+                { "Message" , message}
+            };
+
+            string requestOutput = JsonConvert.SerializeObject(result, Formatting.Indented);
+            successRequest.Output = requestOutput;
+            try
+            {
+                await client.SendTaskSuccessAsync(successRequest);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("ERROR : SendFailureAsync : " + error.Message);
+                Console.WriteLine("ERROR : SendFailureAsync : " + error.StackTrace);
+            }
+            await Task.CompletedTask;
+        }
+    }
+    class CaseDetails
+    {
+        public String customerContact { get; set; } = "";
     }
 }
